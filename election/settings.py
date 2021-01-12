cb971b4420688701f4d3b0ab0bef7232b635f8ba
ibrahim <bigibraeh@gmail.com>
2021-01-12 19:16:49 +0000

    make it better

--- election/settings.py
+++ election/settings.py
@@ -30,7 +30,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'n%!s8!ha0w^be$fn-mi#9m$^d6m89su2ux$_!fv^99phy=k*kt')
 DEBUG = bool(os.environ.get('DJANGO_DEBUG', 'False') != 'False')
 
-ALLOWED_HOSTS = ['djanpolls.herokuapp.com', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['https://djanpolls.herokuapp.com/','djanpolls.herokuapp.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
