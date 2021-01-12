5f3758a320bd8ff4444f9937d03eb3d36f421ea4
ibrahim <bigibraeh@gmail.com>
2021-01-12 18:45:31 +0000

    make it better4

--- election/settings.py
+++ election/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # DEBUG = True
 
 SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'n%!s8!ha0w^be$fn-mi#9m$^d6m89su2ux$_!fv^99phy=k*kt')
-DEBUG = bool(os.environ.get('DJANGO_DEBUG', '') != 'False')
+DEBUG = bool(os.environ.get('DJANGO_DEBUG', 'True') != 'False')
 
 ALLOWED_HOSTS = ['*']
 
